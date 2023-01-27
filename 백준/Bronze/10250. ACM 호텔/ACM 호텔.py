650fe96f35cfabe5c388dac879ef1cb4db276188
n_joid <72697791+nakisara01@users.noreply.github.com>
2023-01-28 01:23:10 +0900

    [Bronze III] Title: ACM 호텔, Time: 44 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
@@ -0,0 +1,12 @@
+import sys
+
+T=int(input())
+
+for i in range(T):
+    H, W, N=map(int, sys.stdin.readline().split())
+    floor=N%H
+    room=N//H+1
+    if floor==0:
+        floor=H
+        room-=1
+    print(floor*100+room)
