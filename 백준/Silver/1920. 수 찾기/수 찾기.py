62bde12c2dfb534fcd3dd7be4d9aa45732dfc92b
n_joid <72697791+nakisara01@users.noreply.github.com>
2025-01-20 21:16:17 +0900

    [Silver IV] Title: 수 찾기, Time: 432 ms, Memory: 49152 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -0,0 +1,29 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+A = list(map(int, input().split()))
+if len(A) != N:
+    print("List size error")
+    exit()
+M = int(input())
+find = list(map(int, input().split()))
+if len(find) != M:
+    print("List size error")
+    exit()
+    
+A.sort()
+
+def binary_search(array, target, start, end):
+    while start <= end:
+        mid = (start + end) // 2
+        if array[mid] == target:
+            return 1
+        elif array[mid] > target:
+            end = mid - 1
+        else:
+            start = mid + 1
+    return 0
+
+for i in find:
+    print(binary_search(A, i, 0, N-1))
