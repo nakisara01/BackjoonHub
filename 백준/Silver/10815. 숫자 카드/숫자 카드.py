d99a8f6dffe7e6d82f0e6b6fd7ef5448bbb9411b
n_joid <72697791+nakisara01@users.noreply.github.com>
2026-01-08 02:11:16 +0900

    [Silver V] Title: 숫자 카드, Time: 444 ms, Memory: 241072 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.py
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -2,33 +2,14 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-A = list(map(int, input().split()))
-if len(A) != N:
-    print("List size error")
-    exit()
+S = set(map(int, input().rstrip().split()))
 M = int(input())
-find = list(map(int, input().split()))
-if len(find) != M:
-    print("List size error")
-    exit()
-result = []
-    
-A.sort()
+deck = list(map(int, input().rstrip().split()))
 
-find_ = [0 for i in find]
+for i in range(M):
+    if deck[i-1] in S:
+        deck[i-1] = 1
+    else:
+        deck[i-1] = 0
 
-def binary_search(array, target, start, end):
-    while start <= end:
-        mid = (start + end) // 2
-        if array[mid] == target:
-            return 1
-        elif array[mid] > target:
-            end = mid - 1
-        else:
-            start = mid + 1
-    return 0
-
-for i in find:
-    result.append(binary_search(A, i, 0, N-1))
-
-print(*result)
+print(*deck)
