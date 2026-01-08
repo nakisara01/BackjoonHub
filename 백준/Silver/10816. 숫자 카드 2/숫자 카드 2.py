ac90f2cad99f49d903378e35e02aa3963e6cdf79
n_joid <72697791+nakisara01@users.noreply.github.com>
2026-01-08 20:13:12 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 512 ms, Memory: 245896 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -2,40 +2,15 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-A = list(map(int, input().split()))
-if len(A) != N:
-    print("List size error")
-    exit()
+cards = list(map(int, input().split()))
+
 M = int(input())
-find = list(map(int, input().split()))
-if len(find) != M:
-    print("List size error")
-    exit()
+count = list(map(int, input().split()))
 
-A.sort()
+cnt = {}
 
-def binary_search(array, target, start, end, find_lower):
-    result = -1
-    while start <= end:
-        mid = (start + end) // 2
-        if array[mid] == target:
-            result = mid
-            if find_lower:
-                end = mid - 1
-            else:
-                start = mid + 1
-        elif array[mid] < target:
-            start = mid + 1
-        else:
-            end = mid - 1
-    return result
-
-def count_occurrences(array, target, n):
-    lower = binary_search(array, target, 0, n - 1, find_lower=True)
-    if lower == -1:
-        return 0
-    upper = binary_search(array, target, 0, n - 1, find_lower=False)
-    return upper - lower + 1
-
-for num in find:
-    print(count_occurrences(A, num, N), end=' ')
+for card in cards:
+    cnt[card] = cnt.get(card, 0) + 1
+    
+for x in count:
+    print(cnt.get(x, 0), end=' ')
