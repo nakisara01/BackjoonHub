913bda00b25245c1a0d94d7d779b1602625bb5e1
n_joid <72697791+nakisara01@users.noreply.github.com>
2026-01-14 12:26:29 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 648 ms, Memory: 66060 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,27 +1,28 @@
 import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
 
-vertex, line = map(int, sys.stdin.readline().split())
-graph = [[] for _ in range(vertex + 1)]
-visited = [False] * (vertex + 1)
+N, M = map(int, input().split())
+graph = [[] for _ in range(N + 1)]
+visited = [False] * (N + 1)
 
-for _ in range(line):
-    a, b = map(int, sys.stdin.readline().split())
-    graph[a].append(b)
-    graph[b].append(a)
+for i in range(1, M + 1):
+    x, y = map(int, input().split())
+    graph[x].append(y)
+    graph[y].append(x)
 
 cnt = 0
 
-def DFS(graph, start, visited):
-    stack = [start]
-    while stack:
-        node = stack.pop()
-        if not visited[node]:
-            visited[node] = True
-            stack.extend(graph[node])
+def DFS(start):
+    visited[start] = True
+    
+    for i in graph[start]:
+        if not visited[i]:
+            DFS(i)
 
-for i in range(1, vertex + 1):
+for i in range(1, N + 1):
     if not visited[i]:
-        DFS(graph, i, visited)
         cnt += 1
+        DFS(i)
 
 print(cnt)
