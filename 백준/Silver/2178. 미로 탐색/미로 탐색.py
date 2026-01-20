2dadd14e49e23d0211b9b2249a76833f76ebc029
n_joid <72697791+nakisara01@users.noreply.github.com>
2026-01-20 20:08:48 +0900

    [Silver I] Title: 미로 탐색, Time: 116 ms, Memory: 114360 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -4,29 +4,30 @@ input = sys.stdin.readline
 from collections import deque
 
 N, M = map(int, input().split())
-graph = [list(map(int, input().strip())) for _ in range(N)]
-dist = [[-1] * M for _ in range(N)]
 
-dx = [1, 0, -1, 0]
-dy = [0, -1, 0, 1]
+graph = [list(map(int, input().strip())) for _ in range(N)]
+dist = [[-1 for _ in range(M)] for _ in range(N)]
+
+ny = [1, -1, 0, 0]
+nx = [0, 0, 1, -1]
 
 def bfs():
     q = deque()
-    q.append((0, 0))
+    q.append((0,0))
     dist[0][0] = 1
 
     while q:
-        ey, ex = q.popleft()
+        y, x = q.popleft()
+
         for i in range(4):
-            ny = ey + dy[i]
-            nx = ex + dx[i]
+            dy = y + ny[i]
+            dx = x + nx[i]
 
-            if 0 <= ny < N and 0 <= nx < M:
-                if ny + 1 == N and nx + 1 == M:
-                    dist[ny][nx] = dist[ey][ex] + 1
-                    return dist[ny][nx]
-                elif graph[ny][nx] == 1 and dist[ny][nx] == -1:
-                    dist[ny][nx] = dist[ey][ex] + 1
-                    q.append((ny, nx))
+            if 0 <= dy < N and 0 <= dx < M:
+                if graph[dy][dx] == 1 and dist[dy][dx] == -1:
+                    dist[dy][dx] = dist[y][x] + 1
+                    q.append((dy, dx))
 
-print(bfs())
+bfs()
+
+print(dist[N - 1][M - 1])
